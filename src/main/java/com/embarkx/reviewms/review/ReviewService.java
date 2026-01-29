f74b0c625c4b904461e7debba36f234dd660b8d4
Kumari Sonika <kumarisonika1929@gmail.com>
2026-01-29 15:23:36 +0530

    first commit

---
+++ src/main/java/com/embarkx/reviewms/review/ReviewService.java
@@ -0,0 +1,66 @@
+package com.embarkx.reviewms.review;
+
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReviewService {
+
+    private ReviewRepository reviewRepository;
+
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+
+    }
+
+    List<Review> getAllReviews(Long companyId){
+        return reviewRepository.findByCompanyId(companyId);
+
+    }
+
+
+    boolean addReview(Long companyId, Review review){
+        try {
+            review.setCompanyId(companyId);
+            reviewRepository.save(review);
+            return true;
+        } catch (RuntimeException e) {
+            return false;
+        }
+    }
+
+    Review getReview(Long reviewId){
+       return reviewRepository.getReferenceById(reviewId);
+    }
+
+
+    public boolean updateReview(Long reviewId, Review updatedReview){
+        Review review = reviewRepository.findById(reviewId).orElse(null);
+        if(review!=null){
+            review.setTitle(updatedReview.getTitle());
+            review.setDescription(updatedReview.getDescription());
+            review.setRating(updatedReview.getRating());
+            review.setCompanyId(updatedReview.getCompanyId());
+            reviewRepository.save(review);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    public boolean deleteReview(Long reviewId){
+        Review review = reviewRepository.findById(reviewId).orElse(null);
+        if(review!=null){
+            reviewRepository.delete(review);
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
