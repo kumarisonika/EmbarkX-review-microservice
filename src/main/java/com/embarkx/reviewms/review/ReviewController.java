0ba4430fd747d2b31954f6329e9b1773bf794b61
Kumari Sonika <kumarisonika1929@gmail.com>
2026-02-06 00:03:49 +0530

    rabbitmq, review sending, config

--- src/main/java/com/embarkx/reviewms/review/ReviewController.java
+++ src/main/java/com/embarkx/reviewms/review/ReviewController.java
@@ -1,5 +1,6 @@
 package com.embarkx.reviewms.review;
 
+import com.embarkx.reviewms.review.messaging.ReviewMessageProducer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +11,11 @@ import java.util.List;
 @RequestMapping("/reviews")
 public class ReviewController {
     private ReviewService reviewService;
+    private ReviewMessageProducer reviewMessageProducer;
 
-    public ReviewController(ReviewService reviewService) {
+    public ReviewController(ReviewService reviewService, ReviewMessageProducer reviewMessageProducer) {
         this.reviewService = reviewService;
+        this.reviewMessageProducer=reviewMessageProducer;
     }
 
 
@@ -23,7 +26,9 @@ public class ReviewController {
 
     @PostMapping()
     public ResponseEntity<String> addReview(@RequestParam Long companyId, @RequestBody Review review){
+
         if(reviewService.addReview(companyId, review)){
+            reviewMessageProducer.sendMessage(review);
             return new ResponseEntity<>("Review added successfully.", HttpStatus.OK);
         }
         else{
@@ -53,5 +58,10 @@ public class ReviewController {
         return new ResponseEntity<>("Review not deleted", HttpStatus.NOT_FOUND);
     }
 
+    @GetMapping("/averageRating")
+    public Double getAverageReview(@RequestParam Long companyId ){
+        List<Review> reviewList = reviewService.getAllReviews(companyId);
+        return reviewList.stream().mapToDouble(Review::getRating).average().orElse(0.0);
+    }
 
 }
